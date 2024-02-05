bf97e1f489478031a68ca73646ecebc835d81127
Pangauwin <65062595+Pangauwin@users.noreply.github.com>
2024-02-05 15:29:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-var exec = require('child_process').exec;
-
-
-let command = prompt('command :')
-
-exec(command,
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
+console.log("Hello world !");
